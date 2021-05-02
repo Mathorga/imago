1e7001509c10cab833c30aa3c2c41c9e112eac46
Mathorga <michelettiluka@gmail.com>
2021-05-02 19:12:17 +0200

    Refactored propagate function

--- src/imago.c
+++ src/imago.c
@@ -1,16 +1,33 @@
 #include "imago.h"
 
-void initColumn(struct Corticolumn* column, uint32_t neuronsNum) {}
+void initColumn(struct Corticolumn* column, uint32_t neuronsNum) {
+    uint32_t synapsesPerNeuron = 10;
 
-void propagate(uint8_t* propagationTimes, uint32_t* indexes, uint8_t* progresses, uint32_t synapsesNum) {
+    // Allocate neurons.
+    column->neuronsNum = neuronsNum;
+    column->neurons = (struct Neuron*) malloc(column->neuronsNum * sizeof(struct Neuron));
+
+    // Allocate synapses.
+    column->synapsesNum = synapsesPerNeuron * neuronsNum;
+    column->synapses = (struct Synapse*) malloc(column->synapsesNum * sizeof(struct Synapse));
+
+    // Initialize neurons with default values.
+    for (uint32_t i = 0; i < neuronsNum; i++) {
+        column->neurons[i].threshold = DEFAULT_THRESHOLD;
+    }
+}
+
+void propagate(struct Corticolumn* column) {
     // Loop through synapses.
-    for (uint32_t i = 0; i < synapsesNum; i++) {
-        if (progresses[i] != SPIKE_DELIVERED && progresses[i] < propagationTimes[i]) {
-            // Increment progress if less than propagationTime and not alredy delivered.
-            progresses[i]++;
-        } else if (progresses[i] >= propagationTimes[i]) {
+    for (uint32_t i = 0; i < column->synapsesNum; i++) {
+        if (column->synapses[i].progress != SPIKE_DELIVERED &&
+            column->synapses[i].progress < column->synapses[i].propagationTime) {
+            // Increment progress if less than propagationTcolumnime and not alredy delicolumnvered.
+            column->synapses[i].progress++;
+        } else if (column->synapses[i].progress >= column->synapses[i].propagationTime) {
             // Set progress to SPIKE_DELIVERED if propagation time is reached.
-            progresses[i] = SPIKE_DELIVERED;
+        } else if (column->synapses[i].progress >= column->synapses[i].propagationTime) {
+            column->synapses[i].progress = SPIKE_DELIVERED;
         }
     }
 }
--- src/imago.h
+++ src/imago.h
@@ -17,6 +17,9 @@ Copyright (C) 2021 Luka Micheletti
 
 #define DECAY_RATE 1
 
+// Default threshold value is 0.8.
+#define DEFAULT_THRESHOLD 0xCCu
+
 struct Neuron {
     // Threshold value. The neuron fires if value goes above it.
     uint8_t threshold;
@@ -50,22 +53,18 @@ struct Corticolumn {
     // Actual neurons in the corticolumn. The size is defined by neuronsNum.
     struct Neuron* neurons;
 
-    // Amount of synapses per neuron. The size is defined by neuronsNum.
-    uint32_t* synapsesNums;
+    // Amount of synapses in the corticolumn.
+    uint32_t synapsesNum;
 
-    // Synapses in the corticolumn. This is an array of synapses (more than one for each neuron). The size is defined by neuronsNum.
-    // Each element is an array of actual synapses. Each size is defined by the corresponding element in synapsesNum.
-    struct Synapses** synapses;
+    // Synapses in the corticolumn. This size is defined by synapsesNum.
+    struct Synapse* synapses;
 };
 
 // Initializes the given Corticolumn with default values.
 void initColumn(struct Corticolumn* column, uint32_t neuronsNum);
 
 // Propagates synapse spikes to post-synaptic neurons.
-void propagate(uint8_t* propagationTimes,
-               uint32_t* indexes,
-               uint8_t* progresses,
-               uint32_t synapsesNum);
+void propagate(struct Corticolumn* column);
 
 // Increments neuron values with spikes from input synapses and decrements them by decay.
 void increment(int8_t* neuronValues,
