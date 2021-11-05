1990ed4414536e153cc26bf4eff63146240a73ca
Luka Micheletti <michelettiluka@gmail.com>
2021-11-06 00:41:48 +0100

    Updated dependencies install scripts

--- samples/install_deps.sh
+++ samples/install_deps.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install C++ compiler.
+sudo apt install g++
+
 # Install sfml for graphics.
 sudo apt install libsfml-dev
 sudo apt install libncurses-dev
+
+printf "\nDependencies installed!\n"
